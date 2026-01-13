92022ca7db18f8c922c126fe49db13171daa937d
Rohit Kumar <rohitdhankhar7347@gmail.com>
2026-01-13 15:39:07 +0530

    Done Basic Setup

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const app = express();
+const apiRoutes  = require('./routes/index');
 const bodyParser = require('body-parser');
 const {PORT} = require('./config/serverConfig');
+const db = require('./models/index');
  setupServer=()=>{
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended:true}));
+    app.use('/api',apiRoutes);
+
    app.listen(PORT,()=>{
     console.log("Server started on",PORT);
-   })
+   });
+   if(process.env.DB_SYNC){
+    db.sequelize.sync({alter:true});
+   }
  }
  setupServer();
---
+++ src/routes/v1/index.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/',(req,res)=>{console.log("Hello");res.send("Hello")});
+
+module.exports = router;
