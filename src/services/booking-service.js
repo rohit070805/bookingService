564480dfdd9ad28ee39317cc71d8f7aae957ca77
Rohit Kumar <rohitdhankhar7347@gmail.com>
2026-01-22 22:46:07 +0530

    updated booking creation to retreive dynamic data and send mails and create dynamic tickets for every user

--- src/services/booking-service.js
+++ src/services/booking-service.js
@@ -1,10 +1,11 @@
 const { default: axios } = require('axios');
 const {BookingRepository} = require('../repository/index');
-const {FLIGHT_SERVICE_PATH} =require('../config/serverConfig');
+const {FLIGHT_SERVICE_PATH,USER_SERVICE_PATH} =require('../config/serverConfig');
 const {publishMessage} = require('../utils/messageQueue');
 const {REMINDER_BINDING_KEY} = require('../config/serverConfig');
 const { ServiceError } = require('../utils/errors');
 const services = require('.');
+
 class BookingService{
     constructor(){
         this.bookingRepository= new BookingRepository();
@@ -33,25 +34,40 @@ class BookingService{
             await axios.patch(updateFlightRequestUrl,{totalSeats:flightData.totalSeats - booking.noOfSeats});
 
             const finalBooking= await this.bookingRepository.update(booking.id,{status:'Booked'});
-        //     const userId = finalBooking.userId;
-        //     let getUserRequestUrl = `${USER_SERVICE_PATH}/api/v1/users/${userId}`;
-        //    const user =  await axios.get(getUserRequestUrl);
+            // getting User
+            const userId = finalBooking.userId;
+            console.log(userId);
+            let getUserRequestUrl = `${USER_SERVICE_PATH}/api/v1/users/${userId}`;
+            const user =  await axios.get(getUserRequestUrl);
+            // getting airports
+            
+               let getdepartureAirportrequestURL = `${FLIGHT_SERVICE_PATH}/api/v1/airport/${flightData.departureAirportId}`;
+                let getarivalAirportrequestURL = `${FLIGHT_SERVICE_PATH}/api/v1/airport/${flightData.arrivalAirportId}`;
+              const departureAirport = (await axios.get(getdepartureAirportrequestURL)).data.data;
+              const arrialAirport =(await axios.get(getarivalAirportrequestURL)).data.data;
+             
            const payLoad = {
             data:{
                 mailFrom:"airlineHelpline@gamil.com",
-                mailTo:"rohitdhankhar7347@gmail.com",
+                mailTo:user.data.data.email,
                 mailSubject:"Booking confirmation Mail",
-                mailBody:`Dear user this is to inform you that your booking is confirmed`
+                mailBody:`Dear user this is to inform you that your booking from ${departureAirport.name} to ${arrialAirport.name} at ${flightData. departureTime } is confirmed.
+                Thank You`
             },
             service:'SEND_BASIC_MAIL'
            };
             publishMessage(channel,REMINDER_BINDING_KEY,JSON.stringify(payLoad));
+
+            // add in queue to create a reminder ticket
+            const departureDate = new Date(flightData.departureTime);
+            const notificationTime = new Date(departureDate.getTime() - 2 * 60 * 60 * 1000);
            const ticketPayload={
             data:{
                 subject:"Ticket Reminder Mail",
-                content:`Dear user this is to inform you that you have a booking at 20:38:00`,
-                recepientEmail:"rohitdhankhar7347@gmail.com",
-                notificationTime:new Date('2026-01-22T20:38:00')
+                content:`Dear user this is to remind you that you have a booking from ${departureAirport.name} to ${arrialAirport.name} at ${flightData.departureTime }.
+                Thank You`,
+                recepientEmail:user.data.data.email,
+                notificationTime:notificationTime
             },
             service:'CREATE_TICKET',
            };
